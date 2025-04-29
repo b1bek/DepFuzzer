49f783223014f249669ae1207181ea4eed65d240
0xlildoudou <67382584+0xlildoudou@users.noreply.github.com>
2025-04-29 08:11:44 +0000

    fix(misc): add specific url for gradle

--- utils/misc.py
+++ utils/misc.py
@@ -12,10 +12,18 @@ def dependency_exists(name, provider, session):
     Method used to check if a dependency is deprecated or not claimed
     """
     try:
-        package = urllib.parse.quote(name,safe='')
-        output = session.get(f"https://deps.dev/_/s/{provider}/p/{package}/v/",
-                            timeout=10)
-        return output.status_code != 404
+        if "gradle" in provider :
+            groupId, artifactId = name.split(':')[0], name.split(':')[1]
+            output = session.get(f'https://search.maven.org/solrsearch/select?q=g:{groupId}+AND+a:{artifactId}&core=gav&rows=20&wt=json',
+                                timeout=10)
+            if output.json()['response']['numFound'] != 0:
+                return output
+            
+        else:
+            package = urllib.parse.quote(name,safe='')
+            output = session.get(f"https://deps.dev/_/s/{provider}/p/{package}/v/",
+                                timeout=10)
+            return output.status_code != 404
     except Exception:
         print("[-] We have been rate limited, going to sleep for 5 minutes.")
         sleep(300) #this means the API drop our requests
@@ -27,9 +35,16 @@ def recover_dependencies(name, version, provider, session):
     Method used to return all dependencies of a dependency
     """
     try:
-        package = urllib.parse.quote(name,safe='')
-        version = re.sub('[^0-9A-Za-z\-\.]+', '', version)
-        return session.get(f"https://deps.dev/_/s/{provider}/p/{package}/v/{version}/dependencies"
+        if "gradle" in provider :
+            groupId, artifactId = name.split(':')[0], name.split(':')[1]
+            output = session.get(f'https://search.maven.org/solrsearch/select?q=g:{groupId}+AND+a:{artifactId}+AND+v:{version}&core=gav&rows=20&wt=json',
+                                timeout=10)
+            if output.json()['response']['numFound'] != 0:
+                return output
+        else:
+            package = urllib.parse.quote(name,safe='')
+            version = re.sub('[^0-9A-Za-z\-\.]+', '', version)
+            return session.get(f"https://deps.dev/_/s/{provider}/p/{package}/v/{version}/dependencies"
                             , timeout=10)
     except Exception:
         print("[-] We have been rate limited, going to sleep for 5 minutes.")
